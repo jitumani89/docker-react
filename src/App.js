12bceb61a9fbe8bc469280c46b184ec43db46db7
Jitu Mani Das <jitumanidas@Jitus-MacBook-Pro.local>
2020-02-08 12:39:09 +1100

    Committed on feature branch

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React by jitu
+          Learn React on feature branch
         </a>
       </header>
     </div>
